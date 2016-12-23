e984abc76743ca2bb5a3d69503d73359516b70b6
TseWayne <tselove123@163.com>
2016-12-23 15:27:31 +0800

    test config

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -42,7 +42,7 @@ $config = array(
 			'DB_PORT'   => 3306, // 端口
 			'DB_PREFIX' => '', // 数据库表前缀 
 			'DB_CHARSET'=> 'utf8', // 字符集
-			'DB_DEBUG'  =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志 3.2.3新增
+			'DB_DEBUG'  =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志 3.2.3新增sss
 
 
 		); 
